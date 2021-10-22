8c134ce28f387f74a9cfdfe7e42cde06b5e1e1cb
ravi-methods <83820221+ravi-methods@users.noreply.github.com>
2021-10-22 08:55:54 +0100

    FT2-865: Save my progress page with validation. (#13)
    * FT2-865: Save my progress page with validation.

---
+++ DFC.App.SkillsHealthCheck/Enums/SaveMyProgressOption.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DFC.App.SkillsHealthCheck.Enums
+{
+    public enum SaveMyProgressOption
+    {
+        [Description("None")]
+        None,
+        [Description("Send me an email with a link")]
+        Email,
+        [Description("Get a reference code")]
+        ReferenceCode,
+    }
+}
