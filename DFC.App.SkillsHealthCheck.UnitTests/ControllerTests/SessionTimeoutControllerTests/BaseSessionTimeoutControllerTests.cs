3a2559c44d26a362aaa534c8fb0a381fcea7feeb
hnandra <32364605+hnandra@users.noreply.github.com>
2024-02-05 11:31:49 +0000

    Added unit tests for SessionTimeoutController

---
+++ DFC.App.SkillsHealthCheck.UnitTests/ControllerTests/SessionTimeoutControllerTests/BaseSessionTimeoutControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using DFC.App.SkillsHealthCheck.Controllers;
+using DFC.App.SkillsHealthCheck.Models;
+using DFC.Compui.Sessionstate;
+using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+
+namespace DFC.App.SkillsHealthCheck.UnitTests.ControllerTests.SessionTimeoutControllerTests
+{
+    public class BaseSessionTimeoutControllerTests
+    {
+        protected ILogger<SessionTimeoutController> Logger { get; } = A.Fake<ILogger<SessionTimeoutController>>();
+
+        protected ISessionStateService<SessionDataModel> SessionStateService { get; } = A.Fake<ISessionStateService<SessionDataModel>>();
+
+        protected IOptions<SessionStateOptions> SessionStateOptions { get; } = Options.Create(new SessionStateOptions());
+
+        protected SessionTimeoutController BuildController(string mediaTypeName)
+        {
+            var httpContext = new DefaultHttpContext();
+
+            httpContext.Request.Headers[HeaderNames.Accept] = mediaTypeName;
+
+            var controller = new SessionTimeoutController(Logger, SessionStateService, SessionStateOptions)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = httpContext,
+                },
+                TempData = new TempDataDictionary(httpContext, A.Fake<ITempDataProvider>()),
+            };
+
+            return controller;
+        }
+    }
+}
---
+++ DFC.App.SkillsHealthCheck.UnitTests/ControllerTests/SessionTimeoutControllerTests/SessionTimeoutControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using DFC.App.SkillsHealthCheck.Controllers;
+using DFC.App.SkillsHealthCheck.ViewModels;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Xunit;
+using DFC.App.SkillsHealthCheck.ViewModels.SessionTimout;
+
+namespace DFC.App.SkillsHealthCheck.UnitTests.ControllerTests.SessionTimeoutControllerTests
+{
+    [Trait("Category", "Session Timeout Controller Unit Tests")]
+    public class SessionTimeoutControllerTests : BaseSessionTimeoutControllerTests
+    {
+        [Fact]
+        public void HtmlHeadRequestReturnsSuccess()
+        {
+            using var controller = BuildController(MediaTypeNames.Text.Html);
+
+            var result = controller.HtmlHead();
+
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var viewModel = viewResult.ViewData.Model.Should().BeAssignableTo<HtmlHeadViewModel>().Which;
+            Assert.Equal($"{SessionTimeoutController.PageTitle} | {SessionTimeoutController.DefaultPageTitleSuffix}", viewModel.Title);
+        }
+
+        [Fact]
+        public void BreadCrumbRequestReturnsSuccess()
+        {
+            using var controller = BuildController(MediaTypeNames.Text.Html);
+
+            var result = controller.Breadcrumb();
+
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var viewModel = viewResult.ViewData.Model.Should().BeAssignableTo<BreadcrumbViewModel>().Which;
+            viewModel.Breadcrumbs.Should().NotBeNull();
+            viewModel.Breadcrumbs.Should().HaveCount(2);
+            viewModel.Breadcrumbs![0].Title.Should().Be("Home");
+            viewModel.Breadcrumbs![1].Title.Should().Be("Skills assessment");
+        }
+
+        [Theory]
+        [InlineData("/skills-health-check/home")]
+        public async Task BodyGetRequestReturnsSuccessAndCorrectReturnLink(string expectedReturnLink)
+        {
+            using var controller = BuildController(MediaTypeNames.Text.Html);
+
+            var result = controller.Body();
+
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var model = viewResult.ViewData.Model.Should().NotBeNull()
+                .And.BeOfType<BodyViewModel>().Which;
+
+            model.HomePageUrl.Should().Be(expectedReturnLink);
+        }
+    }
+}
