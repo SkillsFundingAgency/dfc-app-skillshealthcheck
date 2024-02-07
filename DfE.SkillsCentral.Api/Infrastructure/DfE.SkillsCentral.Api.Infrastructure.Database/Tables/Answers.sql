17d6b32e011fd3cb5be27246ec8dea8caba879c7
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-07 20:49:45 +0000

    Added sql tables

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.Database/Tables/Answers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tblAnswers]
+(
+	[AnswerId] INT NOT NULL PRIMARY KEY,
+	[QuestionID] INT NULL,
+	[AnswerValue] TEXT, /* is this how we know the correct answer? if not, what is? */
+	[AnswerText] TEXT,
+	[ImageSource] TEXT, 
+    [AssessmentId] INT NULL
+)
---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.Database/Tables/Assessment.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblAssessment]
+(
+	[AssessmentId] INT NOT NULL PRIMARY KEY,
+	[AssessmentType] TEXT NOT NULL, /* what is this? */
+	[AssessmentTitle] TEXT NOT NULL,
+	[AssessmentSubtitle] TEXT NOT NULL,
+	[AssessmentIntroduction] TEXT NOT NULL,
+	[QualificationLevelNumber] INT NOT NULL,
+	[AccessibilityLevelNumber] INT NOT NULL,
+)
