6020bfde7e4edda2a87f78ca103d00e8c78bd868
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-07 20:49:45 +0000

    Added sql tables

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.Database/Tables/Questions.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tblQuestions]
+(
+	[QuestionId] INT NOT NULL PRIMARY KEY,
+	[AssessmentId] INT NOT NULL,
+	[QuestionTitle] TEXT,
+	[QuestionNumber] INT,
+	[QuestionText] TEXT,
+	[QuestionDataHTML] TEXT,
+	[ImageTitle] TEXT,
+	[ImageCaption] TEXT,
+	[ImageURL] TEXT
+)
---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.Database/Tables/SkillsDocuments.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tblSkillsDocument]
+(
+	[SkillsDocumentId] INT NOT NULL PRIMARY KEY,
+	[SkillsDocumentTitle] TEXT,
+	[CreatedAt] DATETIME,
+	[CreatedBy] TEXT,
+	[UpdatedAt] DATETIME,
+	[UpdatedBy] TEXT,
+	[DeletedAt] DATETIME,
+	[DeletedBy] TEXT,
+	[ExpiresTimespan] INT,
+	[ExpiresType] INT, /* ??? */
+	[XMLValueKeys] TEXT, /* ??? */
+	[LastAccessed] DATETIME
+)
