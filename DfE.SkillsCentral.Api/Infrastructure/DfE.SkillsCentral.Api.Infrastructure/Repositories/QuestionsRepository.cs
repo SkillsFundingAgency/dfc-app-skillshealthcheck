8d76662d278c7ba5f5e0bbff7e083a5a0255fa09
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-09 13:49:11 +0000

    implemented answers and questions repositories

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Repositories/QuestionsRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Domain.Models;
+using DFC.SkillsCentral.Api.Infrastructure.Queries;
+using System.Data;
+
+namespace DFC.SkillsCentral.Api.Infrastructure.Repositories
+{
+    public class AssessmentsRepository : IAssessmentsRepositiry
+    {
+
+        private readonly DatabaseContext dbContext;
+        public AssessmentsRepository(DatabaseContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IReadOnlyList<Assessment>> GetAllAsync()
+        {
+            using (var connection = dbContext.CreateConnection())
+            {
+                connection.Open();
+                var result = await connection.QueryAsync<Assessment>(AssessmentsQueries.AllAssessments);
+                return result.ToList();
+            }
+        }
+
+        public async Task<Assessment> GetByIdAsync(int assessmentId)
+        {
+            using (var connection = dbContext.CreateConnection())
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<Assessment>(AssessmentsQueries.AssessmentById, new { AssessmentId = assessmentId});
+                return result;
+            }
+        }
+    }
+}
