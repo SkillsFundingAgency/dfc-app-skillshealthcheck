fee7f8fa03cdbe3ea81b434f75bdbd508bde5cdd
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-09 13:51:28 +0000

    removed unncessary attribute

--- DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/DatabaseContext.cs
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/DatabaseContext.cs
@@ -12,7 +12,6 @@ namespace DFC.SkillsCentral.Api.Infrastructure
     public class DatabaseContext
     {
         private readonly IConfiguration _configuration;
-        private readonly string _connectionString;
 
         public DatabaseContext(IConfiguration configuration)
         {
