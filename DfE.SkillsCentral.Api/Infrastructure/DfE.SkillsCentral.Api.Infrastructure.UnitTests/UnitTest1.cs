243e7271dfc1d79059864d1ba8ddb9d316fbb907
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-09 13:51:57 +0000

    added packages and started implementing test scenarios

--- DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.UnitTests/UnitTest1.cs
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure.UnitTests/UnitTest1.cs
@@ -1,11 +1,25 @@
+using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Infrastructure;
+using DFC.SkillsCentral.Api.Infrastructure.Repositories;
+using Moq;
+
 namespace DfE.SkillsCentral.Api.Infrastructure.UnitTests
 {
-    public class UnitTest1
+    public class AssessmentsRepositoryTests
     {
-        [Fact]
-        public void Test1()
+        private readonly AssessmentsRepository _assessmentsRepository;
+        private readonly Mock<DatabaseContext> _dbContext = new Mock<DatabaseContext>(); 
+        public AssessmentsRepositoryTests() 
         {
+            _assessmentsRepository = new AssessmentsRepository(_dbContext.Object);
+        }
 
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnAssessment_WhenAssessmentExists()
+        {
+            var assessmentId = new int();
+            
+            var assessment = await _assessmentsRepository.GetByIdAsync(assessmentId);
         }
     }
 }
