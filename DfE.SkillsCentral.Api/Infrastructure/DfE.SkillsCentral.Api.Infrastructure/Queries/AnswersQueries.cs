1c230680e7a38b2244d828c8c81f135791a30895
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-09 13:49:11 +0000

    implemented answers and questions repositories

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/AnswersQueries.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DFC.SkillsCentral.Api.Infrastructure.Queries
+{
+    public static class AssessmentsQueries
+    {
+        public static string AllAssessments => "SELECT * FROM [Assessments] (NOLOCK)";
+
+        public static string AssessmentById => "SELECT * FROM [Assessments] (NOLOCK) WHERE [AssessmentId] = @AssessmentId";
+    }
+}
+
