a7f188e1395945af66017a8ffeabb6a9e49b146e
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-13 13:04:19 +0000

    started implementing the SkillsDocumentRepository

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/SkillsDocumentsQueries.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DFC.SkillsCentral.Api.Infrastructure.Queries
+{
+    public static class SkillsDocumentsQueries
+    {
+        public static string GetSkillsDocumentByIdAsync => "SELECT * FROM [SkillsDocuments] (NOLOCK) WHERE [SkillsDocumentId] = @SkillsDocumentId";
+
+        public static string GetSkillsDocumentByReferenceCodeAsync => "SELECT * FROM [SkillsDocuments] (NOLOCK) WHERE [ReferenceCode] = @ReferenceCode";
+
+        public static string InsertSkillsDocumentAsync => 
+            "INSERT INTO [SkillsDocuments] (SkillsDocumentTypeSysId, SkillsDocumentTitle, CreatedAtDate, CreatedBy, ExpiresTimespan, ExpiresType, XMLValueKeys, LastAccess, ReferenceCode)" +
+            "VALUES (@SkillsDocumentTypeSysId, @SkillsDocumentTitle, @CreatedAtDate, @CreatedBy, @ExpiresTimespan, @ExpiresType, @XMLValueKeys, @LastAccess, @ReferenceCode)";
+
+        public static string UpdateSkillsDocument =>
+            "UPDATE [SkillsDocuments] SET SkillsDocumentTitle = @SkillsDocumentTitle, UpdatedAtDate = @UpdatedAtDate, UpdatedBy = @UpdatedBy, XMLValueKeys = @XMLValueKeys, " +
+            "ExpiresTimespan = @ExpiresTimespan, ExpiresType = @ExpiresType, LastAccess	= @LastAccess WHERE [SkillsDocumentId] = @SkillsDocumentId";
+
+    }
+}
+
