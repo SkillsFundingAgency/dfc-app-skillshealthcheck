8a537b9b39a0c1f118eb3838066cc7dc8d6a2c63
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-13 13:04:19 +0000

    started implementing the SkillsDocumentRepository

---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Repositories/SkillsDocumentsRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Domain.Models;
+using DFC.SkillsCentral.Api.Infrastructure.Queries;
+using System.Data;
+
+namespace DFC.SkillsCentral.Api.Infrastructure.Repositories
+{
+    public class SkillsDocumentsRepository : ISkillsDocumentsRepository
+    {
+        private readonly DatabaseContext dbContext;
+        public SkillsDocumentsRepository(DatabaseContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<string> AddAsync(SkillsDocument skillsDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SkillsDocument> GetByIdAsync(int skillsDocumentId)
+        {
+            using (var connection = dbContext.CreateConnection())
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<SkillsDocument>(SkillsDocumentsQueries.GetSkillsDocumentByIdAsync, new { SkillsDocumentId = skillsDocumentId });
+                return result;
+            }
+        }
+
+        public async Task<SkillsDocument> GetByReferenceCodeAsync(string referenceCode)
+        {
+            using (var connection = dbContext.CreateConnection())
+            {
+                connection.Open();
+                var result = await connection.QuerySingleOrDefaultAsync<SkillsDocument>(SkillsDocumentsQueries.GetSkillsDocumentByReferenceCodeAsync , new { ReferenceCode = referenceCode });
+                return result;
+            }
+        }
+
+        public Task<string> UpdateAsync(SkillsDocument skillsDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        // AddSkillsDocument
+        // UpdateSkillsDocumment
+
+
+    }
+}
