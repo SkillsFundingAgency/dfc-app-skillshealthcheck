e808488c171f8e33b81974020865b636c22cfd28
Amandeep Sandhu <github@purpleduckmedia.com>
2024-02-28 22:41:01 +0000

    AD-172190 - Added Job Families entitys and repositories

---
+++ DfE.SkillsCentral.Api/Domain/DfE.SkillsCentral.Api.Domain/Models/InterestArea.cs
@@ -0,0 +1,7 @@
+﻿namespace DfE.SkillsCentral.Api.Domain.Models;
+
+public class InterestArea
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Repositories/JobFamiliesRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using DFC.SkillsCentral.Api.Infrastructure;
+using DfE.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DfE.SkillsCentral.Api.Domain.Models;
+
+namespace DfE.SkillsCentral.Api.Infrastructure.Repositories;
+
+public class JobFamiliesRepository : IJobFamiliesRepository
+{
+    private readonly DatabaseContext dbContext;
+
+    public JobFamiliesRepository(DatabaseContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<IReadOnlyList<JobFamily>> GetAllAsync()
+    {
+        var query = @"
+            SELECT 
+                jf.*, 
+                ia.Id AS InterestAreaId, 
+                ia.Name AS InterestAreaName 
+            FROM 
+                JobFamilies jf
+            LEFT JOIN 
+                JobFamiliesInterestAreas jfia ON jf.Id = jfia.JobFamilyId
+            LEFT JOIN 
+                InterestAreas ia ON jfia.InterestAreaId = ia.Id";
+
+        using var connection = dbContext.CreateConnection();
+        var jobFamilies = (await connection.QueryAsync<JobFamily, InterestArea, JobFamily>(
+                query,
+                (jobFamily, interestArea) =>
+                {
+                    jobFamily.InterestAreas.Add(interestArea);
+                    return jobFamily;
+                },
+                splitOn: "InterestAreaId")
+            )
+            .Distinct()
+            .ToList();
+
+        return jobFamilies;
+    }
+}
