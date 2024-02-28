eba5df2c677d034b131b19caecb4c78aef48452d
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-28 14:14:54 +0000

    updated AnswerById query with new Id name

--- DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/AnswersQueries.cs
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/AnswersQueries.cs
@@ -3,9 +3,9 @@ namespace DFC.SkillsCentral.Api.Infrastructure.Queries
 {
     public static class AnswersQueries
     {
-        public static string AllAnswersByQuestionId => "SELECT * FROM [Answers] WHERE [QuestionId] = @QuestionId";
+        public static string AllAnswersByQuestionId => "SELECT * FROM [Answers] WHERE QuestionId = @QuestionId";
 
-        public static string AnswerById => "SELECT * FROM [Answers] WHERE [AnswersId] = @AnswersId";
+        public static string AnswerById => "SELECT * FROM [Answers] WHERE Id = @Id";
 
 
     }
