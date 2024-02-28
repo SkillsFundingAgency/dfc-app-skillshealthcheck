63c7aed56a74e57b5d14444ee95c81fe09f21d09
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-02-28 13:42:10 +0000

    updated Answers queries to reflect recent changes

--- DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/AnswersQueries.cs
+++ DfE.SkillsCentral.Api/Infrastructure/DfE.SkillsCentral.Api.Infrastructure/Queries/AnswersQueries.cs
@@ -3,11 +3,9 @@ namespace DFC.SkillsCentral.Api.Infrastructure.Queries
 {
     public static class AnswersQueries
     {
-        public static string AllAnswersByQuestionId => "SELECT * FROM [Answers] (NOLOCK) WHERE [QuestionId] = @QuestionId";
+        public static string AllAnswersByQuestionId => "SELECT * FROM [Answers] WHERE [QuestionId] = @QuestionId";
 
-        public static string AllAnswersByAssessmentId => "SELECT * FROM [Answers] (NOLOCK) WHERE [AssessmentId] = @AssessmentId";
-
-        public static string AnswerById => "SELECT * FROM [Answers] (NOLOCK) WHERE [AnswersId] = @AnswersId";
+        public static string AnswerById => "SELECT * FROM [Answers] WHERE [AnswersId] = @AnswersId";
 
 
     }
