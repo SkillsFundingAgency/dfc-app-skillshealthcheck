32b1ff1489e53e1c021d0610765a28f207be5da8
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-01 16:34:01 +0000

    updated assessments service to return list of all questions (and answers) and implemented save method

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services/Services/AsessmentsService.cs
@@ -0,0 +1,59 @@
+﻿using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Application.Interfaces.Services;
+using DFC.SkillsCentral.Api.Domain.Models;
+using DfE.SkillsCentral.Api.Application.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DfE.SkillsCentral.Api.Application.Services.Services
+{
+    public class AsessmentsService : IAssessmentsService
+    {
+        private readonly IAssessmentsRepository _assessmentsRepository;
+        private readonly IQuestionsRepository _questionsRepository;
+        private readonly IAnswersRepository _answersRepository;
+        private readonly ISkillsDocumentsRepository _skillsDocumentsRepository;
+
+
+        public AsessmentsService(IAssessmentsRepository assessmentsRepository, IQuestionsRepository questionsRepository, IAnswersRepository answersRepository, ISkillsDocumentsRepository skillsDocumentsRepository)
+        {
+            _assessmentsRepository = assessmentsRepository;
+            _questionsRepository = questionsRepository;
+            _answersRepository = answersRepository;
+            _skillsDocumentsRepository = skillsDocumentsRepository;
+        }
+
+        public async Task<AssessmentQuestions?> GetAssessmentQuestions(string assessmentType)
+        {
+            var assessmentQuestions = new AssessmentQuestions();
+
+            assessmentQuestions.Assessment = await _assessmentsRepository.GetByTypeAsync(assessmentType);
+            if (assessmentQuestions.Assessment == null)
+                return default;
+            var questions = await _questionsRepository.GetAllByAssessmentIdAsync(assessmentQuestions.Assessment.Id);
+            foreach ( var question in questions)
+            {
+                var answers = await _answersRepository.GetAllByQuestionIdAsync(question.Id);
+                var questionAnswers = new QuestionAnswers()
+                {
+                    Question = question,
+                    Answers = answers    
+                };
+
+                assessmentQuestions.Questions.Add(questionAnswers);
+            }
+
+            return assessmentQuestions;
+        }
+
+        public async Task SaveSkillsDocument(SkillsDocument skillsDocument)
+        {
+            await _skillsDocumentsRepository.UpdateAsync(skillsDocument);   
+        }
+    }
+
+    
+}
