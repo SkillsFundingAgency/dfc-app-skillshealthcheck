3a38a1c228d2e1f9539dfd271e26cac07b47aaab
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-01 16:35:29 +0000

    implemented create method for skills documents service and updated get methods to return default if skills document doesnt exist

--- DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Interfaces/Services/ISkillsDocumentsService.cs
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Interfaces/Services/ISkillsDocumentsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DFC.SkillsCentral.Api.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,17 @@ namespace DFC.SkillsCentral.Api.Application.Interfaces.Services
 {
     public interface ISkillsDocumentsService
     {
-        
+
+        Task<SkillsDocument> GetSkillsDocument(int id);
+
+        Task<SkillsDocument> GetSkillsDocumentByReferenceCode(string referenceCode);
+
+        Task<SkillsDocument> CreateSkillsDocument(SkillsDocument skillsDocument);
+
+        void CalculateResults();
+
+        string DownloadDocument();
+
 
     }
 }
---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services/Services/SkillsDocumentsService.cs
@@ -0,0 +1,39 @@
+﻿using DFC.SkillsCentral.Api.Application.Interfaces.Services;
+using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Domain.Models;
+
+namespace DfE.SkillsCentral.Api.Application.Services.Services
+{
+    public class SkillsDocumentsService : ISkillsDocumentsService
+    {
+
+        private readonly ISkillsDocumentsRepository _skillsDocumentsRepository;
+
+        public SkillsDocumentsService(ISkillsDocumentsRepository skillsDocumentsRepository)
+        {
+            _skillsDocumentsRepository = skillsDocumentsRepository;
+        }
+        public async Task<SkillsDocument> CreateSkillsDocument(SkillsDocument skillsDocument)
+        {
+            await _skillsDocumentsRepository.AddAsync(skillsDocument);
+            return await _skillsDocumentsRepository.GetByReferenceCodeAsync(skillsDocument.ReferenceCode);
+        }
+
+        public async Task<SkillsDocument?> GetSkillsDocument(int id)
+        {
+
+            var skillsDocument =  await _skillsDocumentsRepository.GetByIdAsync(id);
+            if (skillsDocument == null)
+                return default;
+            return skillsDocument;
+        }
+
+        public async Task<SkillsDocument?> GetSkillsDocumentByReferenceCode(string referenceCode)
+        {
+            var skillsDocument = await _skillsDocumentsRepository.GetByReferenceCodeAsync(referenceCode);
+            if (skillsDocument == null)
+                return default;
+            return skillsDocument;
+        }
+    }
+}
