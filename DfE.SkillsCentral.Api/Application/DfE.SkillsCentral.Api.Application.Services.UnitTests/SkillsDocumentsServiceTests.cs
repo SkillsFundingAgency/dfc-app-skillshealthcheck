c3ad3a0320eb89b73e28834b3e4e21734b17e4ec
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-04 21:06:43 +0000

    implemented unit tests for SkillsDocuments service

--- DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services.UnitTests/SkillsDocumentsServiceTests.cs
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services.UnitTests/SkillsDocumentsServiceTests.cs
@@ -12,38 +12,58 @@ namespace DfE.SkillsCentral.Api.Application.Services.UnitTests
     {
         private readonly Mock<ISkillsDocumentsRepository> _skillsDocumentsRepository = new Mock<ISkillsDocumentsRepository>();
         private readonly SkillsDocumentsService _documentsService;
-
+        readonly int validId = 1;
+        readonly int invalidId = 2;
+        static readonly string validReferenceCode = "valid";
+        readonly string invalidReferenceCode = "invalid";
+        SkillsDocument skillsDocument = new SkillsDocument() { ReferenceCode = validReferenceCode};
        
         public SkillsDocumentsServiceTests() 
         {
+            _skillsDocumentsRepository.Setup(x => x.GetByIdAsync(validId)).ReturnsAsync(skillsDocument);
+            _skillsDocumentsRepository.Setup(x => x.GetByIdAsync(invalidId)).ReturnsAsync(default(SkillsDocument));
+            _skillsDocumentsRepository.Setup(x => x.GetByReferenceCodeAsync(validReferenceCode)).ReturnsAsync(skillsDocument);
+            _skillsDocumentsRepository.Setup(x => x.GetByReferenceCodeAsync(invalidReferenceCode)).ReturnsAsync(default(SkillsDocument));
 
+            _documentsService = new SkillsDocumentsService(_skillsDocumentsRepository.Object);
 
-            
         }
         [Fact]
         public async Task GetSkillsDocument_ShouldReturnSkillsDocument_WhenValidIdProvided()
         {
-      
+            var document = await _documentsService.GetSkillsDocument(validId);
+            Assert.True(document.Equals(skillsDocument));
         }
 
 
         [Fact]
         public async Task GetSkillsDocument_ShouldReturnDefault_WhenInvalidIdProvided()
         {
-
+            var document = await _documentsService.GetSkillsDocument(invalidId);
+            Assert.Null(document);
         }
 
         [Fact]
         public async Task GetSkillsDocumentByReferenceCode_ShouldReturnSkillsDocument_WhenValidReferenceCodeProvided()
         {
-
+            var document = await _documentsService.GetSkillsDocumentByReferenceCode(validReferenceCode);
+            Assert.True(document.Equals(skillsDocument));
         }
 
 
         [Fact]
         public async Task GetSkillsDocumentByReferenceCode_ShouldReturnDefault_WhenInvalidReferenceCodeProvided()
         {
-
+            var document = await _documentsService.GetSkillsDocumentByReferenceCode(invalidReferenceCode);
+            Assert.Null(document);
         }
+
+        [Fact]
+        public async Task CreateSkillsDocument_ShouldReturnSkillsDocument_WhenSuccessful()
+        {
+            var document = await _documentsService.CreateSkillsDocument(skillsDocument);
+            Assert.True(document.Equals(skillsDocument));
+        }
+
     }
 }
