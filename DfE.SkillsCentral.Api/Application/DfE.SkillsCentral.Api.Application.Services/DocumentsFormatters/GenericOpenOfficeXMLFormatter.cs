eb3c73f72d6995651f3fce1c212373d030cd4276
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-05 16:48:22 +0000

    lift and shift started

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services/DocumentsFormatters/GenericOpenOfficeXMLFormatter.cs
@@ -0,0 +1,174 @@
+﻿
+
+namespace DfE.SkillsCentral.Api.Application.Services.DocumentsFormatters
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Xsl;
+    using System.Security;
+    using DFC.SkillsCentral.Api.Domain.Models;
+    using DfE.SkillsCentral.Api.Application.Interfaces.Models;
+
+    public static class GenericOpenOfficeXMLFormatter
+    {
+        public static byte[] FormatDocumentWithATemplate(SkillsDocument document, string xsltTemplateName)
+        {
+            XmlDocument documentXml = GetSkillsDocumentXml(document);
+            XslTransformation xslt = GetXslTransformationObjects(xsltTemplateName);
+
+            XmlDocument processedDoc = OpenXMLDocumentBuilder.InterpretSkillsDocument(documentXml, xslt);
+
+            string wordTemplateFileName = OpenXMLDocumentBuilder.GetWordTemplateName(processedDoc);
+            byte[] wordDocumentTemplate = GetWordDocumentTemplate(wordTemplateFileName);
+
+            byte[] processedWordDocument = OpenXMLDocumentBuilder.BuildWordDocumentFromTemplate(processedDoc, wordDocumentTemplate);
+
+            return processedWordDocument;
+        }
+
+        private static byte[] GetWordDocumentTemplate(string templateName)
+        {
+            byte[] result;
+            if (Directory.Exists("Templates"))
+            {
+                string filePath = Path.Combine("Templates", templateName);
+                if (File.Exists(filePath))
+                {
+                    result = File.ReadAllBytes(filePath);
+                }
+                else
+                {
+                    throw new Exception(string.Format("Unable to find the word template. Path {0}", filePath));
+                }
+            }
+            else
+            {
+                throw new Exception(string.Format("Unable to find the word document configuration directory. Path {0}", "Templates"));
+            }
+
+            return result;
+        }
+
+        private static XslTransformation GetXslTransformationObjects(string xsltTemplateName)
+        {
+            XslTransformation result = new XslTransformation();
+            if (Directory.Exists("Templates"))
+            {
+                string filePath = Path.Combine("Templates", xsltTemplateName + ".xsl");
+                string extensionObjectConfigurationFilePath = Path.Combine("Templates", xsltTemplateName + "_Extensions.txt");
+
+                if (File.Exists(filePath) && File.Exists(extensionObjectConfigurationFilePath))
+                {
+                    result.TransformationFile = GetTransformationFileObjectFromByteArr(File.ReadAllBytes(filePath));
+                    result.TransformationArgumentList = GetTransformationArgumentList(File.ReadAllText(extensionObjectConfigurationFilePath));
+                }
+                else
+                {
+                    throw new Exception(string.Format("Unable to find the XSLT file. Path {0} Path2: {1}", filePath, extensionObjectConfigurationFilePath));
+                }
+            }
+            else
+            {
+                throw new Exception(string.Format("Unable to find the XSLT configuration directory. Path {0}", "Templates"));
+            }
+
+            return result;
+        }
+
+        private static XsltArgumentList GetTransformationArgumentList(string typeNames)
+        {
+            XsltArgumentList result = new XsltArgumentList();
+
+            if (!string.IsNullOrEmpty(typeNames))
+            {
+                string[] typeNamesArray = typeNames.Split(new string[] { ";#" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string typeName in typeNamesArray)
+                {
+                    Type typeSpecified = Type.GetType(typeName);
+                    if (typeSpecified != null)
+                    {
+                        object extensionObject = Activator.CreateInstance(typeSpecified);
+                        result.AddExtensionObject(string.Format("{2}{0}#{1}", typeSpecified.Module.Name, typeSpecified.Name, "DocumentsFormatters"), extensionObject);
+                    }
+                    else
+                    {
+                        throw new ArgumentException(string.Format("Unable to load specified extension type: {0}", typeSpecified));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static XslCompiledTransform GetTransformationFileObjectFromByteArr(byte[] byteArray)
+        {
+            XslCompiledTransform transformationFile = null;
+            using (MemoryStream mem = new MemoryStream())
+            {
+                mem.Write(byteArray, 0, (int)byteArray.Length);
+
+                mem.Seek(0, SeekOrigin.Begin);
+                XmlReader r = XmlReader.Create(mem);
+#if DEBUG
+                transformationFile = new XslCompiledTransform(true);
+#else
+                transformationFile = new XslCompiledTransform(false);
+#endif
+                transformationFile.Load(r);
+            }
+
+            return transformationFile;
+        }
+
+        private static XmlDocument GetSkillsDocumentXml(SkillsDocument document)
+        {
+            XmlDocument doc = null;
+
+            foreach (SkillsDocumentDataValue dv in document.DataValueKeys)
+            {
+                try
+                {
+                    XmlDocument escapeDocItems = new XmlDocument();
+                    escapeDocItems.LoadXml(dv.Value);
+                    EscapeSpecialCharacters(escapeDocItems.ChildNodes);
+                    dv.Value = escapeDocItems.OuterXml;
+                }
+                catch (Exception ex)
+                {
+                    string Val = SecurityElement.Escape(dv.Value);
+
+                    dv.Value = Val.Replace("\n", "</w:t><w:br/><w:t>");
+                }
+            }
+
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(document.GetType());
+            StringBuilder builder = new StringBuilder();
+            using (TextWriter writer = new StringWriter(builder))
+            {
+                x.Serialize(writer, document);
+                doc = new XmlDocument();
+                doc.LoadXml(builder.ToString());
+            }
+
+            return doc;
+        }
+
+        private static void EscapeSpecialCharacters(XmlNodeList nodes)
+        {
+            foreach (XmlNode node in nodes)
+            {
+                if (node.HasChildNodes)
+                {
+                    EscapeSpecialCharacters(node.ChildNodes);
+                }
+                else if (node.NodeType != XmlNodeType.XmlDeclaration)
+                {
+                    string Val = SecurityElement.Escape(node.InnerText);
+                    node.InnerText = Val.Replace("\n", "</w:t><w:br/><w:t>");
+                }
+            }
+        }
+    }
+}
---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services/DocumentsFormatters/SkillsReportXsltExtension.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using IMS.SkillsCentral.XmlExtensionObjects.SkillsReport.Common;
+using IMS.SkillsCentral.XmlExtensionObjects.SkillsReport.Entity;
+using IMS.SkillsCentral.XmlExtensionObjects.SkillsReport.Resources;
+
+namespace DfE.SkillsCentral.Api.Application.Services.DocumentsFormatters
+{
+    public class SkillsReportXsltExtension
+    {
+        public XPathNavigator GetSHCReportResult(XPathNodeIterator iterator)
+        {
+            iterator.MoveNext();
+            XDocument doc = XDocument.Parse(iterator.Current.OuterXml);
+
+            string qualificationLevel = GetElementValue(doc, Constant.QualificationLevel);
+
+            SkillAreasResult skills = new SkillAreasResult(qualificationLevel,
+                                                           GetElementValue(doc, Constant.SkillAreasType),
+                                                           GetElementValue(doc, Constant.SkillAreasAnswers),
+                                                           GetElementValue(doc, Constant.SkillAreasComplete));
+
+            InterestResult interest = new InterestResult(qualificationLevel,
+                                                         GetElementValue(doc, Constant.InterestType),
+                                                         GetElementValue(doc, Constant.InterestAnswers),
+                                                         GetElementValue(doc, Constant.InterestComplete));
+
+            PersonalStyleResult personalStyle = new PersonalStyleResult(qualificationLevel,
+                                                         GetElementValue(doc, Constant.PersonalType),
+                                                         GetElementValue(doc, Constant.PersonalAnswers),
+                                                         GetElementValue(doc, Constant.PersonalComplete));
+
+            MotivationResult motivation = new MotivationResult(qualificationLevel,
+                                                               GetElementValue(doc, Constant.MotivationType),
+                                                               GetElementValue(doc, Constant.MotivationAnswers),
+                                                               GetElementValue(doc, Constant.MotivationComplete));
+
+            NumericResult numeric = new NumericResult(qualificationLevel,
+                                                     GetElementValue(doc, Constant.NumericEase),
+                                                     GetElementValue(doc, Constant.NumericTiming),
+                                                     GetElementValue(doc, Constant.NumericType),
+                                                     GetElementValue(doc, Constant.NumericAnswers),
+                                                     GetElementValue(doc, Constant.NumericComplete));
+
+            VerbalResult verbal = new VerbalResult(qualificationLevel,
+                                                   GetElementValue(doc, Constant.VerbalEase),
+                                                   GetElementValue(doc, Constant.VerbalTiming),
+                                                   GetElementValue(doc, Constant.VerbalType),
+                                                   GetElementValue(doc, Constant.VerbalAnswers),
+                                                   GetElementValue(doc, Constant.VerbalComplete));
+
+            CheckingResult checking = new CheckingResult(qualificationLevel,
+                                                         GetElementValue(doc, Constant.CheckingEase),
+                                                         GetElementValue(doc, Constant.CheckingTiming),
+                                                         GetElementValue(doc, Constant.CheckingType),
+                                                         GetElementValue(doc, Constant.CheckingAnswers),
+                                                         GetElementValue(doc, Constant.CheckingComplete),
+                                                         GetElementValue(doc, Constant.CheckingEnjoyment));
+
+            MechanicalResult mechanical = new MechanicalResult(qualificationLevel,
+                                                               GetElementValue(doc, Constant.MechanicalEase),
+                                                               GetElementValue(doc, Constant.MechanicalTiming),
+                                                               GetElementValue(doc, Constant.MechanicalType),
+                                                               GetElementValue(doc, Constant.MechanicalAnswers),
+                                                               GetElementValue(doc, Constant.MechanicalComplete),
+                                                               GetElementValue(doc, Constant.MechanicalEnjoyment));
+
+            ShapesResult shapes = new ShapesResult(qualificationLevel,
+                                                   GetElementValue(doc, Constant.SpatialEase),
+                                                   GetElementValue(doc, Constant.SpatialTiming),
+                                                   GetElementValue(doc, Constant.SpatialType),
+                                                   GetElementValue(doc, Constant.SpatialAnswers),
+                                                   GetElementValue(doc, Constant.SpatialComplete),
+                                                   GetElementValue(doc, Constant.SpatialEnjoyment));
+
+            AbstractResult abstractResult = new AbstractResult(qualificationLevel,
+                                                               GetElementValue(doc, Constant.AbstractEase),
+                                                               GetElementValue(doc, Constant.AbstractTiming),
+                                                               GetElementValue(doc, Constant.AbstractType),
+                                                               GetElementValue(doc, Constant.AbstractAnswers),
+                                                               GetElementValue(doc, Constant.AbstractComplete),
+                                                               GetElementValue(doc, Constant.AbstractEnjoyment));
+
+            JobSuggestionResult jobSuggestions = new JobSuggestionResult(qualificationLevel,
+                                                               GetElementValue(doc, Constant.SkillAreasExcludedJobFamilies1),
+                                                               GetElementValue(doc, Constant.SkillAreasExcludedJobFamilies2),
+                                                               GetElementValue(doc, Constant.SkillAreasExcludedJobFamilies3));
+
+            StringBuilder resultXML = new StringBuilder();
+            resultXML.Append(Constant.RootStart);
+
+            resultXML.Append(string.Format(General.SHCFullReportCurrentDateTag, DateTime.Now.ToString(General.SHCFullReportDatetimeFormat)));
+            resultXML.Append(skills.GetResult());
+            resultXML.Append(interest.GetResult());
+            resultXML.Append(personalStyle.GetResult());
+            resultXML.Append(motivation.GetResult());
+            resultXML.Append(numeric.GetResult());
+            resultXML.Append(verbal.GetResult());
+            resultXML.Append(checking.GetResult());
+            resultXML.Append(mechanical.GetResult());
+            resultXML.Append(shapes.GetResult());
+            resultXML.Append(abstractResult.GetResult());
+            jobSuggestions.RankedSkillsCategories = skills.RankedSkillCategories;
+            jobSuggestions.InterestCategories = interest.InterestCategories;
+            jobSuggestions.VerbalComplete = GetboolValue(verbal.Complete);
+            jobSuggestions.NumericComplete = GetboolValue(numeric.Complete);
+            jobSuggestions.CheckingComplete = GetboolValue(checking.Complete);
+            jobSuggestions.SpatialComplete = GetboolValue(shapes.Complete);
+            jobSuggestions.AbstractComplete = GetboolValue(abstractResult.Complete);
+            jobSuggestions.MechanicalComplete = GetboolValue(mechanical.Complete);
+            jobSuggestions.Complete = skills.Complete;
+            resultXML.Append(jobSuggestions.GetResult());
+            resultXML.Append(Constant.RootEnd);
+
+            return XDocument.Parse(resultXML.ToString()).CreateNavigator();
+        }
+
+       
+      
+        private string GetElementValue(XDocument doc, string name)
+        {
+            string returnValue = string.Empty;
+            var items = from item in doc.Descendants(Constant.SkillsDocumentDataValue)
+                        where item.Descendants(Constant.Title).FirstOrDefault().Value == name
+                        select item.Descendants(Constant.Value).FirstOrDefault().Value;
+            if (items.Count() > 0)
+            {
+                returnValue = items.FirstOrDefault().ToString();
+            }
+
+            return returnValue;            
+        }
+
+        private bool GetboolValue(string value)
+        {
+            bool returnval = false;
+            bool.TryParse(value, out returnval);
+            return returnval;
+        }
+    }
+}
