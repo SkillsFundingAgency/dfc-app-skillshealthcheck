944d3ceded5cb5a0bf36ad96f56be11bbd86c679
Amandeep Sandhu <github@purpleduckmedia.com>
2024-03-07 17:25:11 +0000

    Added SkillsDocumentService integration tests - wip

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services.IntegrationTests/TestSetup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Application.Interfaces.Services;
+using DFC.SkillsCentral.Api.Infrastructure;
+using DFC.SkillsCentral.Api.Infrastructure.Repositories;
+using DfE.SkillsCentral.Api.Application.Services.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.SkillsCentral.Api.Application.Services.IntegrationTests
+{
+    public static class TestSetup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            SqlMapper.AddTypeHandler(new DataValuesTypeHandler());
+            services.AddSingleton<IConfiguration>(config);
+            services.AddScoped<DatabaseContext>();
+            services.AddScoped<ISkillsDocumentsRepository, SkillsDocumentsRepository>();
+            services.AddScoped<ISkillsDocumentsService, SkillsDocumentsService>();
+        }
+    }
+}
