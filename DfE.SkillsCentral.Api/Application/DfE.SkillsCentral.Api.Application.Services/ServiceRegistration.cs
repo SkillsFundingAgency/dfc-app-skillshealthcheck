c5fb7f389e960b456aca66a466088f9ac84e68f1
Amandeep Sandhu <github@purpleduckmedia.com>
2024-03-11 21:39:52 +0000

    Refactor to add service registration into shared extension method

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.Services/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using DFC.SkillsCentral.Api.Application.Interfaces.Services;
+using DfE.SkillsCentral.Api.Application.Services.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DfE.SkillsCentral.Api.Application.Services
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAssessmentsService, AssessmentsService>();
+            services.AddScoped<ISkillsDocumentsService, SkillsDocumentsService>();
+        }
+    }
+}
+
