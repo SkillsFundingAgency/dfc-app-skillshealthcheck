6ef952858ebbd37ec1adef8827abb0d9784da7bc
Amandeep Sandhu <github@purpleduckmedia.com>
2024-03-11 23:05:41 +0000

    Added example WebAPI integration test

--- DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Program.cs
+++ DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Program.cs
@@ -1,37 +1,14 @@
-using DFC.SkillsCentral.Api.Infrastructure.Repositories;
-using DFC.SkillsCentral.Api.Infrastructure;
-using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
-using Dapper;
-using DfE.SkillsCentral.Api.Application.Services;
-using DfE.SkillsCentral.Api.Infrastructure;
+using DfE.SkillsCentral.Api.Presentation.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// builder.Services.AddScoped<ILogger, Logger>();
-builder.Services.AddInfrastructureServices();
-builder.Services.AddApplicationServices();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
 
+public partial class Program { }
