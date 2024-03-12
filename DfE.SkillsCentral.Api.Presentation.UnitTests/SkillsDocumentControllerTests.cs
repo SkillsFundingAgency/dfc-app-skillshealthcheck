3d3028f3b6fd5e1092a10767f98e77d5831a2581
Joe Hudson-Small <Joehs@me.com>
2024-03-12 13:04:28 +0000

    skillsdocument controller unit tests init

---
+++ DfE.SkillsCentral.Api.Presentation.UnitTests/SkillsDocumentControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using DFC.SkillsCentral.Api.Application.Interfaces.Repositories;
+using DFC.SkillsCentral.Api.Domain.Models;
+using DfE.SkillsCentral.Api.Application.Services.Services;
+using DfE.SkillsCentral.Api.Presentation.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace DfE.SkillsCentral.Api.Presentation.UnitTests
+{
+    public class SkillsDocumentControllerTests
+    {
+        private readonly ILogger<AssessmentController> logger;
+        //TODO implement
+    }
+}
