09e39428ad4e15ee0f2009fe4b2af691214037cd
Joe Hudson-Small <Joehs@me.com>
2024-03-14 14:27:58 +0000

    PDF generation init commit

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.DocumentFormatter/DocumentsFormatters/DocxTargetFormat.cs
@@ -0,0 +1,15 @@
+﻿namespace DfE.SkillsCentral.Api.Application.DocumentsFormatters
+{
+    /// <summary>
+    /// Defines the document conversion format
+    /// </summary>
+    public enum DocxTargetFormat
+    {
+        PDF     = 1,
+        MHTML   = 2,
+        Doc     = 3,
+        Text    = 4,
+        JPEG    = 5,
+        TIFF    = 6
+    }
+}
