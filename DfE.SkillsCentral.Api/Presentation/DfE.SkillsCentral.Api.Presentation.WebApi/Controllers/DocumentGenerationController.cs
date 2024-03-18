af16e6e7b727d470fd406fa9c3bab694950d6947
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-18 14:37:36 +0000

    implemented DocumentGeneration Controller and integration tests

--- DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Controllers/DocumentGenerationController.cs
+++ DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Controllers/DocumentGenerationController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DFC.SkillsCentral.Api.Application.Interfaces.Services;
+using DfE.SkillsCentral.Api.Application.Services.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DfE.SkillsCentral.Api.Presentation.WebApi.Controllers
 {
@@ -7,12 +10,69 @@ namespace DfE.SkillsCentral.Api.Presentation.WebApi.Controllers
     [ApiController]
     public class DocumentGenerationController : ControllerBase
     {
-        //Implement during #158487:
+        private readonly IDocumentsGenerationService _documentsGenerationService;
+        private readonly ILogger<SkillsDocumentController> _logger;
 
-        //Generate Results
-        //Generate Word Doc
-        //Convert Word to PDF
+        public DocumentGenerationController(IDocumentsGenerationService documentsGenerationService, ILogger<SkillsDocumentController> logger)
+        {
+            _documentsGenerationService = documentsGenerationService;
+            _logger = logger;
+        }
 
-        //Use the Job Families repository when required
+        [HttpGet("docx/{documentId}")]
+        public async Task<IActionResult> GenerateWordDoc(int documentId)
+        {
+            try
+            {
+                
+                var result = await _documentsGenerationService.GenerateWordDoc(documentId);
+                
+                if (result == null)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return File(result, "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"{nameof(GenerateWordDoc)} exception");
+
+                return StatusCode(500, $"An error has occurred");
+            }
+        }
+
+        [HttpGet("pdf/{documentId}")]
+        public async Task<IActionResult> GeneratePDF(int documentId)
+        {
+            try
+            {
+
+                var result = await _documentsGenerationService.GeneratePDF(documentId);
+
+                if (result == null)
+                {
+                    return NoContent();
+                }
+                else
+                {
+                    return File(result, "application/pdf");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"{nameof(GeneratePDF)} exception");
+
+                return StatusCode(500, $"An error has occurred");
+            }
+        }
+
+        
     }
 }
