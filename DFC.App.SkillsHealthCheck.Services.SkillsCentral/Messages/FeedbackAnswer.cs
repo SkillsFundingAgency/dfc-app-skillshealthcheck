ff285f9e73f3b8ee7bd5d2f920175f99e8b27521
LarryOG <43647901+LarryOG@users.noreply.github.com>
2024-03-22 17:21:55 +0000

    deleted old wcf client project

--- DFC.App.SkillsHealthCheck.Services.SkillsCentral/Messages/FeedbackAnswer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DFC.App.SkillsHealthCheck.Services.SkillsCentral.Messages
-{
-    public class FeedbackAnswer
-    {
-        public string AnswerText { get; set; }
-
-        public string AnswerValue { get; set; }
-    }
-}
--- DFC.App.SkillsHealthCheck.Services.SkillsCentral/Messages/GetSkillsDocumentResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using DFC.App.SkillsHealthCheck.Services.SkillsCentral.Models;
-
-namespace DFC.App.SkillsHealthCheck.Services.SkillsCentral.Messages
-{
-    public class GetSkillsDocumentResponse : GenericResponse
-    {
-        public SkillsDocument SkillsDocument { get; set; }
-    }
-}
