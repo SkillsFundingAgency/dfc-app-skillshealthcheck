e977d15b25b9e881b006e0db46df2cd4e3a5d927
Amandeep Sandhu <github@purpleduckmedia.com>
2024-04-02 13:17:45 +0100

    Added Application Insights

--- DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Startup.cs
+++ DfE.SkillsCentral.Api/Presentation/DfE.SkillsCentral.Api.Presentation.WebApi/Startup.cs
@@ -14,6 +14,7 @@ namespace DfE.SkillsCentral.Api.Presentation.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry();
             services.AddControllers();
             services.AddInfrastructureServices();
             services.AddApplicationServices();
