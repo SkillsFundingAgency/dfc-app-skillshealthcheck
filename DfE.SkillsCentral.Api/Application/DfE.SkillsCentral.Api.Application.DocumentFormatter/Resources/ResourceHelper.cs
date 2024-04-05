9560004d6fd63d5b5f94383e0dd34ca980feec9f
Amandeep Sandhu <github@purpleduckmedia.com>
2024-04-05 15:48:13 +0100

    Added license file nuget package

---
+++ DfE.SkillsCentral.Api/Application/DfE.SkillsCentral.Api.Application.DocumentFormatter/Resources/ResourceHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace DfE.SkillsCentral.Api.Application.DocumentFormatter.Resources;
+
+public static class ResourceHelper
+{
+    public static byte[] GetResource(string resourceName)
+    {
+        var stream = Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream($"{Assembly.GetExecutingAssembly().GetName().Name}.Resources.{resourceName}");
+
+        if (stream == null) return Array.Empty<byte>();
+
+        using var memoryStream = new MemoryStream();
+        stream.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
