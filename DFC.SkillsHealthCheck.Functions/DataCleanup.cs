143926d5e27080f39d6924cd3578e3e22f60e719
Joe Hudson-Small <Joehs@me.com>
2024-06-26 13:43:07 +0100

    refactoring

--- DFC.SkillsHealthCheck.Functions/DataCleanup.cs
+++ DFC.SkillsHealthCheck.Functions/DataCleanup.cs
@@ -11,16 +11,32 @@ namespace DFC.SkillsHealthCheck.Functions
 {
     public class DataCleanup
     {
-        private readonly DatabaseContext _dbContext;
-        private readonly string sqlTransaction = "BEGIN TRANSACTION [DeleteOldSkillsDocuments]\r\n  BEGIN TRY\r\n\tDELETE \r\n\tFROM [dbo].[SkillsDocuments]\r\n\tWHERE UpdatedAt <DATEADD(MONTH, -1, GETUTCDATE())\r\n\tCOMMIT TRANSACTION [DeleteOldSkillsDocuments]\r\n  END TRY\r\n  BEGIN CATCH\r\n        ROLLBACK TRANSACTION [DeleteOldSkillsDocuments];\r\n        THROW\r\n  END CATCH  ";
+        private readonly DatabaseContext _dbContext; 
+        private readonly int monthsToKeep = 12;
+
+        private dynamic timeToLive;
+        private dynamic sqlTransaction;
 
         public DataCleanup(DatabaseContext dbContext)
         {
             _dbContext = dbContext;
+            timeToLive = DateTime.UtcNow.AddMonths(-monthsToKeep);
+            sqlTransaction =
+            $@"BEGIN TRANSACTION [DeleteOldSkillsDocuments]
+               BEGIN TRY
+                 DELETE FROM [dbo].[SkillsDocuments]
+                 WHERE UpdatedAt <DATEADD(MONTH, -{monthsToKeep}, GETUTCDATE())
+                 or CreatedAt <DATEADD(MONTH, -{monthsToKeep}, GETUTCDATE())
+                 COMMIT TRANSACTION [DeleteOldSkillsDocuments]
+               END TRY
+               BEGIN CATCH
+                 ROLLBACK TRANSACTION [DeleteOldSkillsDocuments];
+                 THROW
+               END CATCH";
         }
 
         [FunctionName("DataCleanup")]
-        public async Task Run([TimerTrigger("00 45 10 * * *")] TimerInfo timer, ILogger log)
+        public async Task Run([TimerTrigger("00 00 03 * * *")] TimerInfo timer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed daily at 03:00 UTC i.e. at: {DateTime.Now} server-time.");
             //note: when running this locally on machine this uses local time; on azure this uses UTC.
@@ -31,14 +47,12 @@ namespace DFC.SkillsHealthCheck.Functions
                 return;
             }
 
-            var timeToLive = DateTime.UtcNow.AddMonths(-12);
-
-            log.LogInformation($"Running the daily stored procedure to delete SkillsDocuments records not updated in the last 12 months i.e. since: {timeToLive.ToShortDateString()}");
+            log.LogInformation($"Running the daily stored procedure to delete SkillsDocuments records not updated in the last {monthsToKeep} months i.e. since: {timeToLive.ToShortDateString()}");
 
             using (var connection = _dbContext.CreateConnection())
             {
                 connection.Open();
-                await connection.QueryAsync(sqlTransaction);
+                await connection.QueryAsync(sqlTransaction as string);
             }
         }
     }
